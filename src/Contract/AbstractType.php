26d5d534b2945a36d3ce0a501eb6c1def4df523a
Adrian Jeledintan <adrian.jeledintan@precision-soft.com>
2024-10-16 17:06:03 +0300

    AbstractType::getDefaultName()

--- src/Contract/AbstractType.php
+++ src/Contract/AbstractType.php
@@ -13,8 +13,24 @@ use ReflectionClass;
 
 abstract class AbstractType extends Type
 {
+    /**
+     * can be used if you have multiple databases,
+     * with entities that have the same name,
+     * to easily have distinct types, by using the entity manager name as a prefix
+     */
+    public static function getDefaultNamePrefix(): ?string
+    {
+        return null;
+    }
+
+    public static function getDefaultName(): string
+    {
+        return (static::getDefaultNamePrefix() ?? '') . (new ReflectionClass(static::class))->getShortName();
+    }
+
+    /** @deprecated use the static method {@see AbstractType::getDefaultName()} */
     public function getName(): string
     {
-        return (new ReflectionClass(static::class))->getShortName();
+        return static::getDefaultName();
     }
 }
