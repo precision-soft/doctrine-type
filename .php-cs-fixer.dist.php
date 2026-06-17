1dd6ced9df0f6173773cfaa382b91e4d31620305
Adrian Jeledintan <adrian.jeledintan@gmail.com>
2026-06-17 13:42:44 +0300

    chore: normalize php-cs-fixer config, enforce single-space type declarations, auto-start dev container on commit

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,33 +1,24 @@
 <?php
 
-declare(strict_types=1);
-
 /*
  * Copyright (c) Precision Soft
  */
 
-$header = <<<'HEADER'
-Copyright (c) Precision Soft
-HEADER;
+$header = 'Copyright (c) Precision Soft';
 
 $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__)
-    ->exclude([
-        'var',
-        'vendor',
-    ]);
+    ->exclude(['var', 'node_modules', 'vendor']);
 
-return (new PhpCsFixer\Config())
-    ->setRules([
+return (new PhpCsFixer\Config())->setRules(
+    [
         '@PER-CS2.0' => true,
         '@PER-CS2.0:risky' => true,
-        'cast_spaces' => [
-            'space' => 'none',
-        ],
-        'header_comment' => [
-            'header' => $header,
-        ],
-    ])
+        'header_comment' => ['header' => $header],
+        'cast_spaces' => ['space' => 'none'],
+        'type_declaration_spaces' => true,
+    ],
+)
     ->setRiskyAllowed(true)
     ->setUsingCache(true)
     ->setFinder($finder);
