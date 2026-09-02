b502a987fcf448d31451d74647e01f8cf21da239
Adrian Jeledintan <adrian.jeledintan@gmail.com>
2026-09-02 11:27:01 +0300

    chore(dev): add the example gate hooks

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -11,6 +11,10 @@ $finder = PhpCsFixer\Finder::create()
     ->name('doctrine-type-diagnose')
     ->exclude(['var', 'node_modules', 'vendor']);
 
+if (true === \is_dir(__DIR__ . '/.example')) {
+    $finder->in(__DIR__ . '/.example');
+}
+
 return (new PhpCsFixer\Config())->setRules(
     [
         '@PER-CS2.0' => true,
